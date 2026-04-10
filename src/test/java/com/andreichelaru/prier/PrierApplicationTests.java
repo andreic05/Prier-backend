4eb51c90409d7c15d6d3463d401baae52f928f39
Andrei Chelaru <andreichelaru772@gmail.com>
2026-04-10 21:22:21 +0300

    initialize Spring Boot project

---
+++ src/test/java/com/andreichelaru/prier/PrierApplicationTests.java
@@ -0,0 +1,13 @@
+package com.andreichelaru.prier;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrierApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
