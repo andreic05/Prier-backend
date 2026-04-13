2412582e47f94ea6ec20bb471175df54701924a3
Andrei Chelaru <andreichelaru772@gmail.com>
2026-04-13 23:06:33 +0300

    fix: add equals and hashCode for category entity

--- src/main/java/com/andreichelaru/prier/category/Category.java
+++ src/main/java/com/andreichelaru/prier/category/Category.java
@@ -2,6 +2,7 @@ package com.andreichelaru.prier.category;
 
 import com.andreichelaru.prier.product.Product;
 import jakarta.persistence.*;
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -13,15 +14,18 @@ import java.util.Set;
 @Getter
 @Setter
 @NoArgsConstructor
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
 @Entity
 @Table(name = "categories")
 public class Category {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @EqualsAndHashCode.Include
     private Long id;
 
     @Column(unique = true, nullable = false)
+    @EqualsAndHashCode.Include
     private String name;
 
     private String description;
