ee7ddf8c152c12749a4926b9db1013640b7a4c9e
Andrei Chelaru <andreichelaru772@gmail.com>
2026-04-13 22:33:19 +0300

    feat: add collection dtos and mapper

--- src/main/java/com/andreichelaru/prier/collection/Collection.java
+++ src/main/java/com/andreichelaru/prier/collection/Collection.java
@@ -2,25 +2,28 @@ package com.andreichelaru.prier.collection;
 
 import com.andreichelaru.prier.product.Product;
 import jakarta.persistence.*;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
 import java.time.LocalDateTime;
+import java.util.HashSet;
 import java.util.Set;
 
 @Getter
 @Setter
 @NoArgsConstructor
+@ToString(of = {"id", "name"})
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
 @Entity
 @Table(name = "collections")
 public class Collection {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @EqualsAndHashCode.Include
     private Long id;
 
     @Column(unique = true, nullable = false)
+    @EqualsAndHashCode.Include
     private String name;
 
     private String description;
@@ -35,5 +38,5 @@ public class Collection {
     private LocalDateTime updatedAt;
 
     @ManyToMany(mappedBy = "collections")
-    private Set<Product> products;
+    private Set<Product> products = new HashSet<>();
 }
