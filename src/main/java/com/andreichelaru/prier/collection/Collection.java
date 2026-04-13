00f165e4b68a65f2bb2061f795c9235004451433
Andrei Chelaru <andreichelaru772@gmail.com>
2026-04-13 21:48:15 +0300

    feat: add collection entity
    # Conflicts:
    #	src/main/java/com/andreichelaru/prier/collection/Collection.java

--- src/main/java/com/andreichelaru/prier/collection/Collection.java
+++ src/main/java/com/andreichelaru/prier/collection/Collection.java
@@ -7,7 +7,7 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import java.time.LocalDateTime;
-import java.util.List;
+import java.util.Set;
 
 @Getter
 @Setter
@@ -25,12 +25,15 @@ public class Collection {
 
     private String description;
 
+    @Column(columnDefinition = "json")
+    private String metadata;
+
     @Column(name = "created_at")
     private LocalDateTime createdAt;
 
     @Column(name = "updated_at")
     private LocalDateTime updatedAt;
 
-//    @ManyToMany(mappedBy = "collections")
-//    private List<Product> products;
+    @ManyToMany(mappedBy = "collections")
+    private Set<Product> products;
 }
---
+++ src/main/java/com/andreichelaru/prier/collection/CollectionService.java
@@ -0,0 +1,4 @@
+package com.andreichelaru.prier.collection;
+
+public interface CollectionService {
+}
